525bc2f17a51ca3e0a883d3c1f8fa83d2561dfc2
jose ramirez <cortezluisjose@gmail.com>
2019-09-11 14:48:01 -0700

    first commit

---
+++ lib/genius_books.rb
@@ -0,0 +1,6 @@
+require "genius_books/version"
+
+module GeniusBooks
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/genius_books/version.rb
@@ -0,0 +1,3 @@
+module GeniusBooks
+  VERSION = "0.1.0"
+end
