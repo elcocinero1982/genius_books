15ff2e53c2ec7a3dd09a135e4ed127d3f19dd832
elcocinero1982 <cortezluisjose@gmail.com>
2019-09-15 17:05:39 -0700

    remove require statementsand organize

--- lib/genius_books/book.rb
+++ lib/genius_books/book.rb
@@ -1,5 +1,19 @@
 module GeniusBooks
-  class Book
 
+  class Book
+    attr_accessor :title, :link, :author, :description
+    @@all=[]
+    def initialize(title,link,author,description)
+    @title=title
+    @link=link
+    @author=author
+    @description=description
+    @@all << self
+
+     end
+ #title,author,descripyion and link define
+    def self.all
+     @@all
+    end
   end
 end
--- lib/genius_books/scraper.rb
+++ lib/genius_books/scraper.rb
@@ -1,22 +1,16 @@
-require "nokogiri"
-require "open-uri"
-require "pry"
-require_relative "book"
+
 module GeniusBooks
-  class Scraper
-    doc=Nokogiri::HTML(open("https://www.powells.com/25-books-to-read-before-you-die"))
-    doc.css(a),("attr").each do |card|
-  def self.scrape_books
+class Scraper
+  def self.get_books
+     doc=Nokogiri::HTML(open("https://www.powells.com/25-books-to-read-before-you-die"))
+     doc.css('.bookwrapper').each do |card|
 
-
-
+       title=card.css('img').attr('alt').value
+       link=card.css('a').attr('href').value
+       author= card.css('.author').text
+       description=card.css('.blurb').text
+       GeniusBooks::Book.new(title,link,author,description)
      end
+    end
    end
-  end
-
-
-
-
-
-
-GeniusBooks::Scraper.get_books
+ end
