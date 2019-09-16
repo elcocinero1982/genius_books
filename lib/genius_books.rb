787071877ff6b7e824166200d69d944bd032584d
elcocinero1982 <cortezluisjose@gmail.com>
2019-09-15 17:07:40 -0700

    change relatives

--- lib/genius_books.rb
+++ lib/genius_books.rb
@@ -1,6 +1,12 @@
-require "genius_books/version"
 
-module GeniusBooks
-  class Error < StandardError; end
-  # Your code goes here...
-end
+require "nokogiri"
+require "open-uri"
+require "pry"
+require_relative "./genius_books/book"
+require_relative "./genius_books/scraper"
+require_relative "./genius_books/cli"
+require_relative "./genius_books/version" 
+# module GeniusBooks
+#   class Error < StandardError; end
+#   # Your code goes here...
+# end
